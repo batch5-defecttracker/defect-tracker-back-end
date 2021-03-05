53a2e98ce57b20a70f48121a3f2dae8f621549cb
stharshana <shana3642@gmail.com>
2021-03-05 18:02:54 +1100

    added priority and dto

---
+++ defect-tracker-data-common/src/main/java/com/defect/tracker/data/dto/PriorityDto.java
@@ -0,0 +1,23 @@
+package com.defect.tracker.data.dto;
+
+public class PriorityDto {
+	private Long prioId;
+	private String prioName;
+	
+	
+	public Long getPrioId() {
+		return prioId;
+	}
+	public void setPrioId(Long prioId) {
+		this.prioId = prioId;
+	}
+	public String getPrioName() {
+		return prioName;
+	}
+	public void setPrioName(String prioName) {
+		this.prioName = prioName;
+	}
+	
+	
+
+}
