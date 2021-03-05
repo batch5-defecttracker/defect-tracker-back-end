ab99258fe818f6fc01ac92af7f0aec1fca6d9e7c
Your Name <cude1995son@gmail.com>
2021-03-05 12:28:21 +0530

    login model and dto added

---
+++ defect-tracker-data-common/src/main/java/com/defect/tracker/data/dto/LoginDto.java
@@ -0,0 +1,41 @@
+package com.defect.tracker.data.dto;
+
+public class LoginDto {
+	
+	private String email;
+	private String userName;
+	private String password;
+	private String status;
+	private Long empId;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Long getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+}
