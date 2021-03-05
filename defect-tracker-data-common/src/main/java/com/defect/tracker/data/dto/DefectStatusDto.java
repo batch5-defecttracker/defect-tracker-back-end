251ec6ccd8e682325233b03fd89944e7c4c333f4
unknown <jathuseelan96@gmail.com>
2021-03-05 19:12:22 +1100

    Defect Status added

---
+++ defect-tracker-data-common/src/main/java/com/defect/tracker/data/dto/DefectStatusDto.java
@@ -0,0 +1,21 @@
+package com.defect.tracker.data.dto;
+
+public class DefectStatusDto {
+	
+	private long defStId;
+	private String defStIdName;
+	
+	public long getDefStId() {
+		return defStId;
+	}
+	public void setDefStId(long defStId) {
+		this.defStId = defStId;
+	}
+	public String getDefStIdName() {
+		return defStIdName;
+	}
+	public void setDefStIdName(String defStIdName) {
+		this.defStIdName = defStIdName;
+	}
+
+}
