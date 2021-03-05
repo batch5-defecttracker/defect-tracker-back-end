1344ea45494128735064890996a8db4187d2d6c7
Kajanthuka <kajanthu18@gmail.com>
2021-03-05 16:51:36 +0530

    Second Changes With maping Module Class

--- defect-tracker-data-common/src/main/java/com/defect/tracker/data/entities/Module.java
+++ defect-tracker-data-common/src/main/java/com/defect/tracker/data/entities/Module.java
@@ -1,8 +1,11 @@
 package com.defect.tracker.data.entities;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
@@ -15,20 +18,26 @@ public class Module {
 	
 	@Id
 	@GeneratedValue(strategy  = GenerationType.AUTO)
-	private Long modId;
+	private Long id;
 	private String moduleName;
-	private Long proID;
+	@OneToMany(fetch=FetchType.LAZY )
+	@JoinColumn(name="projectId" ,nullable = false)
+	private Project project;
 	
-	
-	
-	public Long getModId() {
-		return modId;
+
+
+	public Project getProject() {
+		return project;
 	}
-	public void setModId(Long modId) {
-		this.modId = modId;
+	public void setProject(Project project) {
+		this.project = project;
 	}
-	
-	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getModuleName() {
 		return moduleName;
 	}
@@ -37,12 +46,7 @@ public class Module {
 	}
 	
 	
-	public Long getProID() {
-		return proID;
-	}
-	public void setProID(Long proID) {
-		this.proID = proID;
-	}
+	
 	
 	public static long getSerialversionuid() {
 		return serialVersionUID;
