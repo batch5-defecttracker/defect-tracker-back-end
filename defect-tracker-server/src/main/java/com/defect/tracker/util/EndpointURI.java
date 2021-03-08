aad9014541600098813ec4359fec0e0ba9b0d68a
unknown <vvaheeson@gmail.com>
2021-03-08 16:06:44 +1100

    defecttype api added

--- defect-tracker-server/src/main/java/com/defect/tracker/util/EndpointURI.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/util/EndpointURI.java
@@ -8,6 +8,9 @@ public final class EndpointURI {
 	private static final String BASE_API_PATH = "/api/v1/";
 //Employee
 	public static final String EMPLOYEE = BASE_API_PATH + "employee";
+	
+//DefectType
+	public static final String DEFECTTYPE = BASE_API_PATH + "defecttype";
 
 	private EndpointURI() {
 	}
