089b5a43c88c165695d801a38481e718ce37ff32
Vaanathy <sabavaanathy@gmail.com>
2021-03-08 17:16:26 +0530

    validatuion added

--- defect-tracker-server/src/main/java/com/defect/tracker/util/ValidationConstance.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/util/ValidationConstance.java
@@ -2,6 +2,8 @@ package com.defect.tracker.util;
 
 public class ValidationConstance {
 	public static final String EMAIL_EXISTS = "Email already exist";
+	
+	public static final String EMPLOYEE_EMPTY = "Employee not exist";
 
 
 	private ValidationConstance() {
