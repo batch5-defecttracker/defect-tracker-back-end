10b79f7aebe6747a33e9c61a01bd54fab2a94417
Vaanathy <sabavaanathy@gmail.com>
2021-03-09 16:21:28 +0530

    Nathy added

--- defect-tracker-server/src/main/java/com/defect/tracker/controller/EmployeeController.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/controller/EmployeeController.java
@@ -1,14 +1,14 @@
 package com.defect.tracker.controller;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.defect.tracker.data.dto.EmployeeDto;
 import com.defect.tracker.data.entities.Employee;
 import com.defect.tracker.data.mapper.Mapper;
@@ -78,6 +78,17 @@ public class EmployeeController {
 		employeeService.deleteEmployee(id);
 		return new ResponseEntity<Object>(Constants.EMPLOYEE_DELETE_SUCCESS, HttpStatus.OK);
 	}
+	
+	@GetMapping(value= EndpointURI.GET_EMPLOYEE_BY_DESIGNATION )
+	public ResponseEntity<Object> findEmployeeByDesignation(@PathVariable Long designationId){
+		if (!employeeService.isEmployeeExists(designationId)) {
+			return new ResponseEntity<>(new ValidationFailureResponse(ValidationConstance.EMPLOYEE_EMPTY,
+					validationFailureStatusCodes.getEmployeeNotFound()), HttpStatus.BAD_REQUEST);
+		}
+		
+		return new ResponseEntity<Object>(mapper.map(employeeService.findByDes(designationId), EmployeeDto.class), HttpStatus.OK);
+
+	} 
 	
 
 }
--- defect-tracker-server/src/main/java/com/defect/tracker/services/EmployeeService.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/services/EmployeeService.java
@@ -8,11 +8,10 @@ import com.defect.tracker.data.entities.Employee;
 public interface EmployeeService {
 	public void createEmployee(Employee employee);
 	public boolean isEmailAlreadyExist(String email);
-
 	public Optional<Employee> findByFirstName(String firstName);
-
 	public Optional <Employee> findById(Long id);
 	public boolean idExist(Long id);
+	public List<Employee> findByDes(Long id);
 
 
 
