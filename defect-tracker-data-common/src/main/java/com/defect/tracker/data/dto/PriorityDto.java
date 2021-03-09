71714e8f58c2d1f8b79a63c4c3c408fa6d958c3a
unknown <jathuseelan96@gmail.com>
2021-03-09 20:39:57 +1100

    priority updaded

--- defect-tracker-data-common/src/main/java/com/defect/tracker/data/dto/PriorityDto.java
+++ defect-tracker-data-common/src/main/java/com/defect/tracker/data/dto/PriorityDto.java
@@ -1,16 +1,17 @@
 package com.defect.tracker.data.dto;
 
 public class PriorityDto {
-	private Long prioId;
+	private Long Id;
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
 	private String prioName;
 	
 	
-	public Long getPrioId() {
-		return prioId;
-	}
-	public void setPrioId(Long prioId) {
-		this.prioId = prioId;
-	}
+	
 	public String getPrioName() {
 		return prioName;
 	}
