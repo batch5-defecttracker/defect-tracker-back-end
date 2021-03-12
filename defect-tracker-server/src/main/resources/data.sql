9f252ea9a4435ba34ffcb24620926be85835de68
raj-mirabilis <sivakumarthanuraj1@gmail.com>
2021-03-12 10:51:36 +0530

    not neeed statements removed and scripts - SQL - added

--- defect-tracker-server/src/main/resources/data.sql
+++ defect-tracker-server/src/main/resources/data.sql
@@ -8,12 +8,6 @@ INSERT INTO `defect-tracker`.`designation` (`id`, `designation_name`) VALUES ('6
 --Project
 INSERT INTO `defect-tracker`.`project` (`id`, `abbrevation`, `project_name`) VALUES ('1', 'SGIC', 'DefectTrackerSystem');
 INSERT INTO `defect-tracker`.`project` (`id`, `abbrevation`, `project_name`) VALUES ('2', 'SGM', 'SuperMix');
---Module
-INSERT INTO `defect-tracker`.`module` (`id`, `module_name`, `project_id`) VALUES ('1', 'UI_Project_Add', '1');
-INSERT INTO `defect-tracker`.`module` (`id`, `module_name`, `project_id`) VALUES ('2', 'B_UIX_Project', '2');
---SubModule
-INSERT INTO `defect-tracker`.`submodule` (`id`, `submodule_name`, `module_id`) VALUES ('1', 'UI_1', '1');
-INSERT INTO `defect-tracker`.`submodule` (`id`, `submodule_name`, `module_id`) VALUES ('2', 'UI_X', '2');
 --Priority
 INSERT INTO `defect-tracker`.`priority` (`id`, `priority_name`) VALUES ('1', 'Low');
 INSERT INTO `defect-tracker`.`priority` (`id`, `priority_name`) VALUES ('2', 'Medium');
@@ -23,9 +17,11 @@ INSERT INTO `defect-tracker`.`severity` (`id`, `severity_name`) VALUES ('1', 'Lo
 INSERT INTO `defect-tracker`.`severity` (`id`, `severity_name`) VALUES ('2', 'Medium');
 INSERT INTO `defect-tracker`.`severity` (`id`, `severity_name`) VALUES ('3', 'High');
 --Defect Status
-INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('1', 'Low');
-INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('2', 'Medium');
-INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('3', 'High');
+INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('1', 'Open');
+INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('2', 'Closed');
+INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('3', 'Fixes');
+INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('4', 'Reopen');
+INSERT INTO `defect-tracker`.`defect_status` (`id`, `defect_status_name`) VALUES ('5', 'Reject');
 --Defect Type
 INSERT INTO `defect-tracker`.`defecttype` (`id`, `defect_type_name`) VALUES ('1', 'UI');
 INSERT INTO `defect-tracker`.`defecttype` (`id`, `defect_type_name`) VALUES ('2', 'CR');
