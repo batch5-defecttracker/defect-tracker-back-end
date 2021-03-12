105764072e8b142d84925805fa0b819464c138ca
KiNgStOn93 <joyeeking800@gamil.com>
2021-03-12 13:39:41 +0530

    Conflict resloved

--- defect-tracker-server/src/main/java/com/defect/tracker/util/ValidationConstance.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/util/ValidationConstance.java
@@ -2,8 +2,6 @@ package com.defect.tracker.util;
 
 public class ValidationConstance {
 	public static final String EMAIL_EXISTS = "Email already exist";
-	
-	public static final String EMPLOYEE_PHOTO_EMPTY = "Employee Photo is Empty";
 	public static final String PROJECT_DOES_NOT_EXISTS = "Project not Exist";
 
 	public static final String EMPLOYEE_NOT_EXISTS = "Employee does not exist";
@@ -46,6 +44,7 @@ public class ValidationConstance {
 	
 	public static final String DEFECT_STATUS_NOT_EXISTS= "Defect Status not Exists";
 	
+	public static final String EMPLOYEE_PHOTO_EMPTY = "Employee Photo is Empty";
 
 	private ValidationConstance() {
 	}
