37c2523bcdd7668a3ebb35752e353783fcf8b6ba
Vaanathy <sabavaanathy@gmail.com>
2021-04-28 09:21:20 +0530

    priority and defect status controllers corrected

--- defect-tracker-server/src/main/java/com/defect/tracker/controller/PriorityController.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/controller/PriorityController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.defect.tracker.data.dto.PriorityDto;
+import com.defect.tracker.data.mapper.Mapper;
 import com.defect.tracker.services.PriorityService;
 import com.defect.tracker.util.EndpointURI;
 import com.defect.tracker.util.ValidationFailureStatusCodes;
@@ -17,10 +19,11 @@ public class PriorityController {
 	private PriorityService priorityService;
 	@Autowired
 	ValidationFailureStatusCodes validationFailureStatusCodes;
-	
+	@Autowired
+	private Mapper mapper;
 
 	@GetMapping(value = EndpointURI.PRIORITY) 
 	public ResponseEntity<Object> getAllPriority() {
-		return new ResponseEntity<Object>(priorityService.getAllPriority(), HttpStatus.OK);
+		return new ResponseEntity<Object>(mapper.map(priorityService.getAllPriority(), PriorityDto.class), HttpStatus.OK);
 	}
 }
