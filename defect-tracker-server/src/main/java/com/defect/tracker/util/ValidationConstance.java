7619e02238de1053780bfbad891d3d903029fd19
Your Name <k.minuja@gmail.com>
2021-04-30 11:18:33 +0530

    ValidationConstance

--- defect-tracker-server/src/main/java/com/defect/tracker/util/ValidationConstance.java
+++ defect-tracker-server/src/main/java/com/defect/tracker/util/ValidationConstance.java
@@ -1,36 +1,34 @@
 package com.defect.tracker.util;
 
 public class ValidationConstance {
-	public static final String EMAIL_EXISTS = "Email already exist";
-	public static final String EMPLOYEE_NAME_NOT_EXISTS = "Employee name does not exist";
-	public static final String EMPLOYEE_NOT_EXISTS = "Employee not exist";
-	public static final String EMPLOYEE_DOES_NOT_EXISTS = "This employee have not defect";
-	public static final String EMPLOYEEID_EMPTY = "Employeeid not exist";
-	public static final String EMPLOYEE_PHOTO_EMPTY = "Employee Photo is Empty";
-	public static final String PROJECT_DOES_NOT_EXISTS = "Project not Exist";
+	public static final String EMAIL_EXISTS = "Email Already Exist";
+	public static final String EMPLOYEE_NAME_NOT_EXISTS = "Employee Name Not Exist";
+	public static final String EMPLOYEE_NOT_EXISTS = "Employee Not Exist";
+	public static final String EMPLOYEE_DOES_NOT_EXISTS = "This Employee Has Not Defect";
+	public static final String EMPLOYEEID_EMPTY = "EmployeeId Not Exist";
+	public static final String EMPLOYEE_PHOTO_EMPTY = "Employee Photo Is Empty";
+	public static final String PROJECT_DOES_NOT_EXISTS = "Project Not Exist";
 	public static final String PROJECT_EXISTS = "Project  Already Exists";
 	public static final String PROJECT_NAME_EXISTS = "Project Name Already Exists";
-	public static final String EMPLOYEE_NOT_ALLOCATED = "Employee not Allocated";
-	public static final String PROJECT_EMPLOYEE_NOT_AVAILABLE = "Project employee id not exists";
-	public static final String MODULE_NOT_EXISTS = "Module not exist";
+	public static final String EMPLOYEE_NOT_ALLOCATED = "Employee Not Allocated";
+	public static final String PROJECT_EMPLOYEE_NOT_AVAILABLE = "Project EmployeeId Not exists";
+	public static final String MODULE_NOT_EXISTS = "Module Not Exist";
 	public static final String MODULE_ALREADY_EXIST = "Module Already Exists";
-	public static final String SUB_MODULE_NOT_EXISTS = "SubModule not exist";
-	public static final String SUBMODULE_DOES_NOT_EXISTS = "SubModule doesn't Exist ";
-	public static final String SUBMODULE_ALREADY_EXIST = "The Name You enterd SubModule is Already Exists In Module";
-	public static final String DEFECT_EMPTY = "Defect not exist";
-	public static final String DEFECT_DOES_NOT_EXISTS = "Defect not Exist";
-	public static final String DEFECT_NOT_EXISTS = "Defect not Exist";
-	public static final String DEFECT_ID_NOT_EXISTS = "Defect id not Exists";
-	public static final String DEFECT_STATUS_NOT_EXISTS = "Defect Status not Exists";
-	public static final String DEFECT_STATUS_EMPTY = "Defect Status are Empty";
-	public static final String PRIORITY_NOTEXIST = "priority not exist";
-	public static final String SEVERITY_NOTEXIST = "Severity not exist";
-	public static final String DESIGNATION_NOT_EXIST = "Designation not exist";
-	public static final String PASSWORD_DO_NOT_MATCH = "Password do not match";
-	public static final String EMAIL_NOT_EXISTS = "Email not exist";
-	public static final String INVALID_EMAIL = "Email is Wrong !";
-	public static final String INVALID_TOKEN = "Invalid Token !";
-	public static final String TOKEN_EXPIRED = "Token Expired !";
+	public static final String SUB_MODULE_NOT_EXISTS = "SubModule Not Exist";
+	public static final String SUBMODULE_DOES_NOT_EXISTS = "SubModule Not Exist";
+	public static final String SUBMODULE_ALREADY_EXIST = "SubModuleName Already Exists";
+	public static final String DEFECT_EMPTY = "Defect Not Exist";
+	public static final String DEFECT_DOES_NOT_EXISTS = "Defect Not Exist";
+	public static final String DEFECT_NOT_EXISTS = "Defect Not Exist";
+	public static final String DEFECT_ID_NOT_EXISTS = "DefectId Not Exists";
+	public static final String DEFECT_STATUS_NOT_EXISTS = "DefectStatus Not Exists";
+	public static final String DEFECT_STATUS_EMPTY = "DefectStatus Empty";
+	public static final String DESIGNATION_NOT_EXIST = "Designation Not Exist";
+	public static final String PASSWORD_DO_NOT_MATCH = "Password Not matched";
+	public static final String EMAIL_NOT_EXISTS = "Email Not Exist";
+	public static final String INVALID_EMAIL = "Invalid Email";
+	public static final String INVALID_TOKEN = "Invalid Token";
+	public static final String TOKEN_EXPIRED = "Token Expired";
 
 	private ValidationConstance() {
 	}
